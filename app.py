e40775275a36c85c2a33f26d523755c45123309b
yashmane0502-stack <yashmane0502@gmail.com>
2026-02-15 15:16:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ job_title = st.selectbox("Job Title", encoder["Job Title"].classes_)
 years_of_experience = st.number_input("Years of Experience", min_value=0, max_value=65)
 
 
-df = pd.dataFrame({
+df = pd.DataFrame({
     "Age":[age],
     "Gender":[gender],
     "Education Level":[education],
